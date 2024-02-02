9dddafc9018ed24adc4b6b3d723fe51d1483fea2
TaigaTi <tarikabirch@gmail.com>
2024-02-02 09:36:22 -0400

    added matching logic

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
   let cards = document.getElementsByClassName("card");
+  let flipped = [];
 
-  // Flip a card if chosen
   function flipCard() {
     for (let i = 0; i < cards.length; i++) {
       cards[i].addEventListener("click", function () {
-        cards[i].classList.toggle("card-flipped");
+        // Flip a card if chosen
+        if (flipped.length < 2 && !cards[i].classList.contains("card-flipped")) {
+          cards[i].classList.add("card-flipped");
+          flipped.push(cards[i]);
+        }
+
+        // If 2 cards flipped and they aren't a match
+        if (flipped.length == 2 && !getMatch(flipped)) {
+          const card1 = flipped[0];
+          const card2 = flipped[1];
+
+          setTimeout(function () {
+            card1.classList.remove("card-flipped");
+            card2.classList.remove("card-flipped");
+          }, 1000);
+        }
+
+        // If 2 cards are a match
+        if (flipped.length == 2 && getMatch(flipped)) {
+          const card1 = flipped[0];
+          const card2 = flipped[1];
+
+          setTimeout(function () {
+            card1.classList.add("card-flipped");
+            card2.classList.add("card-flipped");
+            card1.removeEventListener("click", flipCard);
+            card2.removeEventListener("click", flipCard);
+          }, 1000);
+        }
+
+        if (flipped.length == 2) {
+          // Reset flip count
+          flipped.length = 0;
+        }
       });
-      
     }
+  }
+
+  function getMatch(flipped) {
+    let image1 = flipped[0].querySelector(".fruits");
+    let image2 = flipped[1].querySelector(".fruits");
+
+    if (image1.getAttribute("src") == image2.getAttribute("src")) {
+      return true;
+    }
+
+    return false;
   }
 
   // Populate the back of the cards with images
